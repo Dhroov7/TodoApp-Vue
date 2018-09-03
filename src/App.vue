da1c7a7bd3061bd41ece835237a8949e36fbd428
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-09-03 18:40:46 +0530

    created a todo app using vue

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <input id="input-box" placeholder="Enter you want to add to your todo list" v-model="newTodo"/>
+    <button v-on:click="addTodo">Add todo</button>
+    <ul>
+      <li v-for="item in todos" v-bind:class="{'completed' : item.done}">
+        {{ item.text }}
+        <input type="checkbox" v-on:change="completeTodo" v-model="item.done"/>
+      </li>
+    </ul>
+    <button v-on:click="removeTodos">Clear Todos</button>
+  </div>
+</template>
+
+<script>
+var id = 3
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Todo App Using Vue.js',
+      newTodo: '',
+      todos: [
+        {id:1,text: 'learn vue',done:false},
+        {id: 2,text: 'make a todo app',done:false},
+        {id:3, text:'test this app',done:false}
+      ]
+    }
+  },
+  methods: {
+    addTodo: function () {
+      var value = this.newTodo
+      if(!value) {
+        return
+      }
+      this.todos.push({
+        id:id,
+        text: value,
+        done:false
+      })
+
+      id++
+
+      this.newTodo = ''
+    },
+    removeTodos: function () {
+      this.todos = []
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+#input-box{
+  width: 40%;
+}
+
+.completed{
+  color: mediumseagreen;
+}
+</style>
